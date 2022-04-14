23dc662427f319ebfeff74fbe5b501377e175c21
Arnaud Le Garrec <legarrecarnaud.92@gmail.com>
2022-04-14 15:17:56 +0200

    configuration de config/initializer/rails_admin.rb

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -3,16 +3,21 @@ RailsAdmin.config do |config|
   ### Popular gems integration
 
   ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
 
   ## == CancanCan ==
   # config.authorize_with :cancancan
 
   ## == Pundit ==
   # config.authorize_with :pundit
+  config.authorize_with do
+    unless current_user.admin?
+      flash[:alert] = 'Sorry, no admin access for you.'
+      redirect_to main_app.root_path
+    end
 
   ## == PaperTrail ==
   # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
