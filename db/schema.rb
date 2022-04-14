c81e2e424b6186413a102bcddddcabd4b489bc91
Arnaud Le Garrec <legarrecarnaud.92@gmail.com>
2022-04-14 17:40:00 +0200

    dans la seed, ajout de Mr patate comme admin

--- db/schema.rb
+++ db/schema.rb
@@ -72,7 +72,7 @@ ActiveRecord::Schema.define(version: 2022_04_14_130722) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "surnom"
     t.string "avatar"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
