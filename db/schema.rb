f602c8df4502f921628b388ab4d892dd96f4c625
Arnaud Le Garrec <legarrecarnaud.92@gmail.com>
2022-04-15 07:29:43 +0200

    adding super admin boolean column to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_14_130722) do
+ActiveRecord::Schema.define(version: 2022_04_15_052715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2022_04_14_130722) do
     t.string "surnom"
     t.string "avatar"
     t.boolean "admin", default: false
+    t.boolean "super_admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
