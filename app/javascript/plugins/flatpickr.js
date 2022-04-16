1b6d377331aa8e1be2867f10729e97892ecc07ce
Arnaud Le Garrec <legarrecarnaud.92@gmail.com>
2022-04-16 23:41:07 +0200

    calendar updated to a range & centered

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,7 +1,18 @@
 import flatpickr from "flatpickr";
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 const initFlatpickr = () => {
-  flatpickr(".datepicker", {});
+  flatpickr("#range_start", {
+    mode: "range",
+    minDate: "today",
+    dateFormat: "Y-m-d",
+    disable: [
+        function(date) {
+            // disable every multiple of 8
+            return !(date.getDate() % 8);
+        }
+    ]
+  });
 }
 
 export { initFlatpickr };
